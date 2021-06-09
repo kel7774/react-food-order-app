d3edf73b6c4646e8cbed8e9c0fbae191785c5ca4
Kelli Landry <landry.kel@gmail.com>
2021-06-09 11:29:51 -0500

    Add context, refs, wrap components, modify cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import HeaderCartButton from '../Layout/HeaderCartButton'
 import mealsImage from '../../assets/meals.jpeg'
 import classes from './Header.module.css'
@@ -8,7 +8,7 @@ const Header = props => {
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='Table full of food' />
---
+++ src/store/CartProvider.js
@@ -0,0 +1,41 @@
+import { useReducer } from 'react'
+import CartContext from './cart-context'
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_CART_ITEM') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return defaultCartState
+}
+
+const CartProvider = props => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_CART_ITEM', item })
+  }
+  const removeItemFromHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE_CART_ITEM', id })
+  }
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromHandler
+  }
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
